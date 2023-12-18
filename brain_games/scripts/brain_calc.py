a9156bbdff8303c3a5e180adbf86b2486513790e
Maksim Uteev <Max.uteev@yandex.ru>
2023-12-18 16:22:11 +0400

    Changed the brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,43 +1,13 @@
-import prompt
-import random
+#!/usr/bin/env python3
 
 
-def say_welcome():
+from brain_games.games.calc import find_correct_answer
+
+
+def main():
     print('Welcome to the Brain Games!')
+    find_correct_answer()
 
 
-say_welcome()
-
-
-def welcome_user():
-    global user_name
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-
-
-welcome_user()
-
-
-def find_correct_answer():
-    print('What is the result of the expression?')
-    i = 0
-    while i < 3:
-        number_1 = random.randint(1, 10)
-        number_2 = random.randint(1, 10)
-        math_operator = random.choice(['+', '-', '*'])
-        math_expression = f'{number_1} {math_operator} {number_2}'
-        result_expression = eval(math_expression)
-        print(f'Question: {math_expression}')
-        user_answer = int(prompt.string('Your answer: '))
-        if user_answer == result_expression:
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print(f'Congratulations, {user_name}!')
-        else:
-            print(
-                f"{user_answer} is wrong answer ;(. Correct answer was {result_expression}.\nLet's try again, {user_name}!")
-            break
-
-
-find_correct_answer()
+if __name__ == '__main__':
+    main()
