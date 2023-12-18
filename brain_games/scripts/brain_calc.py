63853d4d0b7807fb647d656e16d2e833abdd3571
Maksim Uteev <Max.uteev@yandex.ru>
2023-12-18 12:04:29 +0400

    Add some fix

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -22,7 +22,6 @@ def find_correct_answer():
     print('What is the result of the expression?')
     i = 0
     while i < 3:
-        i += 1
         number_1 = random.randint(1, 10)
         number_2 = random.randint(1, 10)
         math_operator = random.choice(['+', '-', '*'])
@@ -32,11 +31,13 @@ def find_correct_answer():
         user_answer = int(prompt.string('Your answer: '))
         if user_answer == result_expression:
             print('Correct!')
+            i += 1
+            if i == 3:
+                print(f'Congratulations, {user_name}!')
         else:
             print(
                 f"{user_answer} is wrong answer ;(. Correct answer was {result_expression}.\nLet's try again, {user_name}!")
             break
-    print(f'Congratulations, {user_name}!')
 
 
 find_correct_answer()
