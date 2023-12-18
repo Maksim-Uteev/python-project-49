7c7a62b7f82f3b73a03557e26baf2207997afe3d
Maksim Uteev <Max.uteev@yandex.ru>
2023-12-18 12:05:26 +0400

    Add some fix

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -23,7 +23,6 @@ def find_correct_answer():
     print('What is the result of the expression?')
     i = 0
     while i < 3:
-        i += 1
         number_1 = random.randint(1, 100)
         number_2 = random.randint(1, 100)
         result_gcd = math.gcd(number_1, number_2)
@@ -31,11 +30,13 @@ def find_correct_answer():
         user_answer = int(prompt.string('Your answer: '))
         if user_answer == result_gcd:
             print('Correct!')
+            i += 1
+            if i == 3:
+                print(f'Congratulations, {user_name}!')
         else:
             print(
                 f"{user_answer} is wrong answer ;(. Correct answer was {result_gcd}.\nLet's try again, {user_name}!")
             break
-    print(f'Congratulations, {user_name}!')
 
 
 find_correct_answer()
