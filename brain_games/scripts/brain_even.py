7742f334994a80dd0b70d637c439264f11fcf2ad
Maksim Uteev <Max.uteev@yandex.ru>
2023-12-18 12:04:59 +0400

    Add some fix

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -22,12 +22,14 @@ def find_correct_answer():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     i = 0
     while i < 3:
-        i += 1
         number = random.randint(1, 100)
         print(f'Question: {number}')
         user_answer = prompt.string('Your answer: ')
         if ((user_answer == 'yes') and (number % 2 == 0)) or ((user_answer == 'no') and (number % 2 != 0)):
             print('Correct!')
+            i += 1
+            if i == 3:
+                print(f'Congratulations, {user_name}!')
         elif user_answer != 'yes' and number % 2 == 0:
             print(
                 f"'{user_answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {user_name}!")
@@ -36,7 +38,6 @@ def find_correct_answer():
             print(
                 f"'{user_answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {user_name}!")
             break
-    print(f'Congratulations, {user_name}!')
 
 
 find_correct_answer()
