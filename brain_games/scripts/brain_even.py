1bd0c5b80b92f11f8ef33818c8d3a41deb051ded
Maksim Uteev <Max.uteev@yandex.ru>
2023-12-18 16:24:54 +0400

    Changed the brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,43 +1,13 @@
-import prompt
-import random
+#!/usr/bin/env python3
 
 
-def say_welcome():
+from brain_games.games.even import find_correct_answer
+
+
+def main():
     print('Welcome to the Brain Games!')
+    find_correct_answer()
 
 
-say_welcome()
-
-
-def welcome_user():
-    global user_name
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-
-
-welcome_user()
-
-
-def find_correct_answer():
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    while i < 3:
-        number = random.randint(1, 100)
-        print(f'Question: {number}')
-        user_answer = prompt.string('Your answer: ')
-        if ((user_answer == 'yes') and (number % 2 == 0)) or ((user_answer == 'no') and (number % 2 != 0)):
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print(f'Congratulations, {user_name}!')
-        elif user_answer != 'yes' and number % 2 == 0:
-            print(
-                f"'{user_answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {user_name}!")
-            break
-        elif user_answer != 'no' and number % 2 != 0:
-            print(
-                f"'{user_answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {user_name}!")
-            break
-
-
-find_correct_answer()
+if __name__ == '__main__':
+    main()
