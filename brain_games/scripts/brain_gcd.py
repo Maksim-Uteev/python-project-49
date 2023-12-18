19ba27f8e0762dad7ff2ae8012eaf9504934c4f5
Maksim Uteev <Max.uteev@yandex.ru>
2023-12-18 16:25:25 +0400

    Changed the brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,42 +1,13 @@
-import prompt
-import random
-import math
+#!/usr/bin/env python3
 
 
-def say_welcome():
+from brain_games.games.gcd import find_correct_answer
+
+
+def main():
     print('Welcome to the Brain Games!')
+    find_correct_answer()
 
 
-say_welcome()
-
-
-def welcome_user():
-    global user_name
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-
-
-welcome_user()
-
-
-def find_correct_answer():
-    print('What is the result of the expression?')
-    i = 0
-    while i < 3:
-        number_1 = random.randint(1, 100)
-        number_2 = random.randint(1, 100)
-        result_gcd = math.gcd(number_1, number_2)
-        print(f'Question: {number_1} {number_2}')
-        user_answer = int(prompt.string('Your answer: '))
-        if user_answer == result_gcd:
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print(f'Congratulations, {user_name}!')
-        else:
-            print(
-                f"{user_answer} is wrong answer ;(. Correct answer was {result_gcd}.\nLet's try again, {user_name}!")
-            break
-
-
-find_correct_answer()
+if __name__ == '__main__':
+    main()
