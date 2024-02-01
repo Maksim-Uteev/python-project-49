14a8c4fc0f77375c66b927fa15a34ecb3ccd9ffb
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-01 11:47:40 +0400

    Fix code

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,26 +1,12 @@
-import prompt
 import random
 
+GAME_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-def find_correct_answer():
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    while i < 3:
-        random_number = random.randint(1, 100)
-        print(f'Question: {random_number}')
-        user_answer = prompt.string('Your answer: ')
-        if is_even(random_number) == user_answer:
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print(f'Congratulations, {user_name}!')
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{is_even(random_number)}'.")
-            print(f"Let's try again, {user_name}!")
-            break
+
+def calculation():
+    question = random.randint(1, 100)
+    result = is_even(question)
+    return question, result
 
 
 def is_even(number):
