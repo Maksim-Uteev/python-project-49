dd32e2b4b7242f1a6be4338e9e7ad92331e0f323
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-01 11:32:22 +0400

    Fix code

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,27 +1,12 @@
-import prompt
 import random
 
+GAME_TASK = 'What is the result of the expression?'
 
-def find_correct_answer():
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print('What is the result of the expression?')
-    i = 0
-    while i < 3:
-        number_1 = random.randint(1, 10)
-        number_2 = random.randint(1, 10)
-        math_operator = random.choice(['+', '-', '*'])
-        math_expression = f'{number_1} {math_operator} {number_2}'
-        result_expression = eval(math_expression)
-        print(f'Question: {math_expression}')
-        user_answer = int(prompt.string('Your answer: '))
-        if user_answer == result_expression:
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print(f'Congratulations, {user_name}!')
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{result_expression}'")
-            print(f"Let's try again, {user_name}!")
-            break
+
+def calculation():
+    number_1 = random.randint(1, 10)
+    number_2 = random.randint(1, 10)
+    math_operator = random.choice(['+', '-', '*'])
+    question = f'{number_1} {math_operator} {number_2}'
+    result = str(eval(question))
+    return question, result
