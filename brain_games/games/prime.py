6fd6b3ea2c9af172ac321d36c895d7df678507d7
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-01 12:18:37 +0400

    Fix code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,26 +1,13 @@
-import prompt
 import random
 
+GAME_TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
-def find_correct_answer():
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    i = 0
-    while i < 3:
-        random_number = random.randint(-1, 8)
-        print(f'Question: {random_number}')
-        user_answer = prompt.string('Your answer: ')
-        if is_prime(random_number) == user_answer:
-            print('Correct!')
-            i += 1
-            if i == 3:
-                print(f'Congratulations, {user_name}!')
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{is_prime(random_number)}'.")
-            print(f"Let's try again, {user_name}!")
-            break
+
+def calculation():
+    random_number = random.randint(-1, 40)
+    question = f'{random_number}'
+    result = is_prime(random_number)
+    return question, result
 
 
 def is_prime(number):
