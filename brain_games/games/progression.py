2db7649d39cd6c2fbb742fb00188eaf419e7460c
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-03 22:52:08 +0400

    Fix code after review

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,15 +1,16 @@
 import random
 
 GAME_TASK = 'What number is missing in the progression?'
+LENGHT_PROGRESSION = 10
 
 
-def calculation():
-    number_1 = random.randint(0, 20)
-    length_progression = 10
-    step_progression = random.randint(0, 10)
-    progression = [number_1 + i
-                   * step_progression for i in range(length_progression)]
-    mutable_element = random.randint(0, 9)
+def get_game():
+    start_number = random.randint(0, 20)
+    step_progression = random.randint(1, 10)
+    stop_number = start_number + LENGHT_PROGRESSION * step_progression
+    progression = list(range(
+        start_number, stop_number, step_progression))
+    mutable_element = random.randint(0, LENGHT_PROGRESSION - 1)
     new_progression = progression.copy()
     new_progression[mutable_element] = '..'
     result_new_progression = ' '.join(map(str, new_progression))
