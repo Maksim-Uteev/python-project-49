6860b7e46da592b4a9ff47d3bea9c299f407efc8
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-03 22:52:45 +0400

    Fix code after review

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,5 +6,5 @@ GAME_TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 def get_game():
     random_number = random.randint(1, 100)
     question = f'{random_number}'
-    result = "yes" if random_number % 2 == 0 else "no"
+    result = 'yes' if random_number % 2 == 0 else 'no'
     return question, result
