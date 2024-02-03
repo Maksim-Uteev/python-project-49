a6744060eb08b26ef5b7ff8b41e3a7a044105abb
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-03 22:49:35 +0400

    Fix code after review

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -4,7 +4,7 @@ import math
 GAME_TASK = 'Find the greatest common divisor of given numbers.'
 
 
-def calculation():
+def get_game():
     number_1 = random.randint(1, 100)
     number_2 = random.randint(1, 100)
     question = f'{number_1} {number_2}'
