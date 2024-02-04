422e6dcec8e2e9838aa55086ec002ed21d68720a
Maksim Uteev <max.uteev@yandex.ru>
2024-02-04 17:15:51 +0400

    Fix code after 5 review

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,17 +4,17 @@ GAME_TASK = 'What number is missing in the progression?'
 LENGHT_PROGRESSION = 10
 
 
-def get_game():
+def get_question_and_answer():
     start_number = random.randint(0, 20)
     step_progression = random.randint(1, 10)
     stop_number = start_number + LENGHT_PROGRESSION * step_progression
-    hidden_element = random.randint(0, LENGHT_PROGRESSION - 1)
+    hidden_index = random.randrange(LENGHT_PROGRESSION)
 
     progression = list(range(
         start_number, stop_number, step_progression))
 
-    correct_answer = str(progression[hidden_element])
-    progression[hidden_element] = '..'
+    correct_answer = str(progression[hidden_index])
+    progression[hidden_index] = '..'
     question = ' '.join(map(str, progression))
 
     return question, correct_answer
