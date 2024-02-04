fc2db9ee62475f46a460f7bd59dd1860859f1819
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-04 11:47:58 +0400

    Fix code after 4 review

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,13 +7,14 @@ GAME_TASK = 'What is the result of the expression?'
 def get_game():
     number_1 = random.randint(1, 10)
     number_2 = random.randint(1, 10)
-    operators = {
-        "+": operator.add,
-        "-": operator.sub,
-        "*": operator.mul
-    }
-    random_key_operator, random_operator = random.choice(
-        list(operators.items()))
-    question = f'{number_1} {random_key_operator} {number_2}'
-    result = str(random_operator(number_1, number_2))
-    return question, result
+
+    math_operator, operation = random.choice([
+        ("+", operator.add),
+        ("-", operator.sub),
+        ("*", operator.mul)
+    ])
+
+    question = f'{number_1} {math_operator} {number_2}'
+    correct_answer = str(operation(number_1, number_2))
+
+    return question, correct_answer
