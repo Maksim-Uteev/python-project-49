5e55ef0acdbc537e374fe7396dac41e66cd2b6d0
Maksim Uteev <Max.uteev@yandex.ru>
2024-02-04 11:47:48 +0400

    Fix code after 4 review

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,16 +8,19 @@ def start_the_game(game_module):
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
+
     print(game_module.GAME_TASK)
-    for round in range(GAME_ROUND):
-        question, result = game_module.get_game()
+
+    for _ in range(GAME_ROUND):
+        question, correct_answer = game_module.get_game()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
-        if user_answer == result:
-            print('Correct!')
-        else:
-            print(f"'{user_answer}' is wrong answer ;(."
-                  f"Correct answer was '{result}'")
-            print(f"Let's try again, {user_name}!")
+
+        if user_answer != correct_answer:
+            print(f"'{user_answer}' is wrong answer ;(.\n"
+                  f"Correct answer was '{correct_answer}'\n"
+                  f"Let's try again, {user_name}!")
             return
+        print('Correct!')
+
     print(f'Congratulations, {user_name}!')
